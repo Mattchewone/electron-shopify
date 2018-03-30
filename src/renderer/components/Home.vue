1cea324f5049fb0b18780271443f2cf6c3e1d17b
Matt <matt@ideajunction.uk>
2018-03-30 22:30:19 +0100

    Added title and print button action

--- src/renderer/components/Home.vue
+++ src/renderer/components/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <v-layout row wrap justify-center id="wrapper">
+    <h1>Unfulfilled Orders</h1>
+
     <v-flex xs12 class="mt-3">
       <v-data-table
         :headers="headers"
         :items="orders"
         hide-actions>
         <template slot="items" slot-scope="props">
-          <td>{{ props.item.id }}</td>
+          <td>{{ props.item.order_number }}</td>
           <td>{{ props.item.email }}</td>
           <td>{{ props.item.financial_status }}</td>
           <td>{{ props.item.confirmed }}</td>
           <td>{{ props.item.total_price }}</td>
-          <td><v-btn small color="primary">PRINT</v-btn></td>
+          <td><v-btn small color="primary" :to="{ name: 'print', params: { id: props.item.order_number } }">PRINT</v-btn></td>
         </template>
       </v-data-table>
     </v-flex>
@@ -26,7 +28,7 @@ export default {
   data () {
     return {
       headers: [
-        { text: 'Order#', value: 'id' },
+        { text: 'Order#', value: 'order_number' },
         { text: 'Email', value: 'email' },
         { text: 'Status', value: 'financial_status' },
         { text: 'Confirmed', value: 'confirmed' },
